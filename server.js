8781da69c8800dc3b2023b2182d86d6b91031646
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-06-29 00:10:34 -0300

    feat: :sparkles: Criando CRUD - Query Parameters

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ server.post("/videos", (request, reply) => {
   return reply.status(201).send();
 });
 
-server.get("/videos", () => {
-  const videos = database.list();
+server.get("/videos", (request) => {
+  const search = request.query.search;
+  const videos = database.list(search);
+
   return videos;
 });
 
@@ -38,6 +40,7 @@ server.put("/videos/:id", (request, reply) => {
 server.delete("/videos/:id", (request, reply) => {
   const videoId = request.params.id;
   database.delete(videoId);
+
   return reply.status(204).send();
 });
 
